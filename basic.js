b828cdb3cb3d9da80a63c051708f3763315f5d71
Michael Shipley <mshply@uiowa.edu>
2024-10-25 18:12:36 -0500

    Added basic.js file to src and created a React app

---
+++ basic.js
@@ -0,0 +1,16 @@
+function MyButton() {
+  return (
+    <button>
+      Click Me!
+    </button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <hi>Welcome to my app</hi>
+      <MyButton />
+    </div>
+  );
+}
